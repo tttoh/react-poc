8cf5b24be5229dd504751b825bcfb439ba309868
tttoh <tttoh@gmail.com>
2020-02-16 22:02:40 +0800

    Updates

---
+++ precache-manifest.8a0e80d97130a2f70fc7b67534fe72d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824ee421f94ae8e8ea3e45a7a97defde",
+    "url": "/react-poc/index.html"
+  },
+  {
+    "revision": "8cee31be398e047810dc",
+    "url": "/react-poc/static/css/main.575aa535.chunk.css"
+  },
+  {
+    "revision": "777764db7a1f046bd8d0",
+    "url": "/react-poc/static/js/2.3c94003d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-poc/static/js/2.3c94003d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cee31be398e047810dc",
+    "url": "/react-poc/static/js/main.225752a8.chunk.js"
+  },
+  {
+    "revision": "d236000bd0dcec47efb8",
+    "url": "/react-poc/static/js/runtime-main.930ad7ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-poc/static/media/logo.5d5d9eef.svg"
+  }
+]);
